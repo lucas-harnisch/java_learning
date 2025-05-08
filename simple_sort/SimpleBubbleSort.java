b17936d775f6e36fa81b9ca074c00ccd2afd1017
Lucas Harnisch <3010081@stud.hs-mannheim.de>
2025-05-08 12:36:38 +0200

    added SortAlgorithm

---
+++ simple_sort/SimpleBubbleSort.java
@@ -0,0 +1,24 @@
+package simple_sort;
+
+public class SimpleBubbleSort {
+
+    public static void bubblesort(int[] int_array) {
+        
+        for (int i = 0; i < int_array.length; i++) {
+            for (int j = 0; j < int_array.length -1; j++) {
+                if (int_array[j] > int_array[j + 1]) {
+
+                    int temp = int_array[j];
+                    int_array[j] = int_array[j +1];
+                    int_array[j + 1] = temp;
+
+
+
+
+                }
+            }
+
+            
+        }
+}
+}
---
+++ simple_sort/SimpleSelectionSort.java
@@ -0,0 +1,5 @@
+package simple_sort;
+
+public class SimpleSelectionSort {
+
+}
